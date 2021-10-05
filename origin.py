88034589eb80b68323f2544d7d19baa38ee4926c
Jhoselinne <jhoselinne.alvarez@uabc.edu.mx>
2021-10-05 16:18:03 -0700

    Cadenas de Python

---
+++ origin.py
@@ -0,0 +1,24 @@
+#MANERAS DE CONCATENAR CADENAS EN PYTHON
+name='jhoselinne alvarez servin'
+age=22
+istall=True
+height=1.70
+
+print(name+" tiene "+str(age)+ " años.")
+
+print("{} tiene {} años.".format(name,age))
+
+print(f"{name} tiene {age+10} años \
+y es alta {istall} y mide {height}")
+
+print(name.upper())
+print(name.lower())
+print(name.title())
+
+print(name*5)
+
+
+
+
+
+
