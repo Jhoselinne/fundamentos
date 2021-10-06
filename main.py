ea21e300e1b038c549cc911e38f2358df8b2156a
Jhoselinne <jhoselinne.alvarez@uabc.edu.mx>
2021-10-05 17:10:56 -0700

    Modulos de Python

---
+++ main.py
@@ -0,0 +1,8 @@
+#import operations
+from operations import * 
+print(sum(10,5))
+print(rest(10,5))
+print(div(10,5))
+print(mensaje)
+
+#print(operations.rest(10,5))
---
+++ operations.py
@@ -0,0 +1,18 @@
+def sum(num1, num2):
+    return num1+num2
+
+def rest(num1, num2):
+    return num1-num2
+
+def mult(num1, num2):
+    return num1*num2
+
+#division entera con 2 //
+def div(num1, num2):
+    return num1//num2
+
+#division decimal
+def div(num1, num2):
+    return num1/num2
+
+mensaje="Hola mundo"
